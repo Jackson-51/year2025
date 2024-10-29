bd9e2d74e101efab28df2e497d4876dab3ee9569
jacksontemi_dev <fabayojackson5@gmail.com>
2024-10-29 15:08:06 +0100

    changed div to Link

--- src/comp.js
+++ src/comp.js
@@ -22,34 +22,34 @@ const Sidebar = () => {
             <div className="sidebars">
             <header>
                 <i className="fa-solid fa-heart"></i>
-                <h4>Team.Co</h4>
+                <h4>Team.Co</h4> 
             </header>
             <div className="items">
                 <span className="slider" style={{top:`${position * 56}px`}}></span>
-                <div className="item" onMouseEnter={() => slide(0)}>
+                <Link to="/" className="item" onMouseEnter={() => slide(0)}>
                     <i className="fa-solid fa-house"></i>
-                    <Link style={{opacity:`${(value)? '0' : '1'}`}}>Home</Link>
-                </div>
-                <div className="item" onMouseEnter={() => slide(1)}>
+                    <p style={{opacity:`${(value)? '0' : '1'}`}}>Home</p>
+                </Link>
+                <Link to="/" className="item" onMouseEnter={() => slide(1)}>
                     <i className="fa-solid fa-gear"></i>
-                    <Link to="/" style={{opacity:`${(value)? '0' : '1'}`}}>Settings</Link>
-                </div>
-                <div className="item" onMouseEnter={() => slide(2)}>
+                    <p style={{opacity:`${(value)? '0' : '1'}`}}>Settings</p>
+                </Link>
+                <Link to="/" className="item" onMouseEnter={() => slide(2)}>
                     <i className="fa-brands fa-windows"></i>
-                    <Link to="/" style={{opacity:`${(value)? '0' : '1'}`}}>Dashboard</Link>
-                </div>
-                <div className="item" onMouseEnter={() => slide(3)}>
+                    <p style={{opacity:`${(value)? '0' : '1'}`}}>Dashboard</p>
+                </Link>
+                <Link to="/" className="item" onMouseEnter={() => slide(3)}>
                     <i className="fa-solid fa-envelope"></i>
-                    <Link to="/" style={{opacity:`${(value)? '0' : '1'}`}}>Mail</Link>
-                </div>
-                <div className="item" onMouseEnter={() => slide(4)}>
+                    <p style={{opacity:`${(value)? '0' : '1'}`}}>Mail</p>
+                </Link>
+                <Link to="/" className="item" onMouseEnter={() => slide(4)}>
                     <i className="fa-solid fa-cloud"></i>
-                    <Link to="/" style={{opacity:`${(value)? '0' : '1'}`}}>Cloud</Link>
-                </div>
-                <div className="item" onMouseEnter={() => slide(5)}>
+                    <p style={{opacity:`${(value)? '0' : '1'}`}}>Cloud</p>
+                </Link>
+                <Link to="/" className="item" onMouseEnter={() => slide(5)}>
                     <i className="fa-solid fa-circle-dot"></i>    
-                    <Link to="/" style={{opacity:`${(value)? '0' : '1'}`}}>Workpiece</Link>
-                </div>
+                    <p style={{opacity:`${(value)? '0' : '1'}`}}>Workpiece</p>
+                </Link>
             </div>
         </div>
         </div>
